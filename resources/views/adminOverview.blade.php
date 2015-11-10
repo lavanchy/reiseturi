c9032498ab2e6c2d8d45af642cca15162bcfb04f
chriskoller08 <chriskoller08@5105edd4-cd02-4104-8b14-f72aa8a08039>
2015-11-10 18:33:47 +0000

    css-fertigstellung von adminOverview

--- resources/views/adminOverview.blade.php
+++ resources/views/adminOverview.blade.php
@@ -3,8 +3,8 @@
 <!--content -->
 @section ('content')
 <div class="container">
-    <h2>Contextual Classes</h2>
-    <p>Contextual classes can be used to color table rows or table cells. The classes that can be used are: .active, .success, .info, .warning, and .danger.</p>            
+    <h2>Admin Übersicht</h2>
+    <p>Die Tabellen werden dynamisch erstellt mit assoc. Die Farben könne verwendet werden, um den Status anzuzeigen, CSS-Technisch kein Problem.</p>
     <table class="table">
         <thead>
             <tr>
@@ -15,27 +15,40 @@
                 <th>Hinfahrt</th>
                 <th>Rückfahrt</th>
                 <th>Buchungen</th>
-                <th>Status</th>
+                
                 <th></th>
             </tr>
         </thead>
         <tbody>
-            <tr class="success">
+            <tr class="info">
                 <td>0001</td>
                 <td>Wien</td>
                 <td>Zürich</td>
                 <td>Bellvue</td>
                 <td>23.12.15</td>
+                <td>24.12.15</td>
+                <td>13</td>
+                <td><button type="button" class="btn btn-default">Bearbeiten</button></td>
+            </tr>
+            <tr class="success">
+                <td>0002</td>
+                <td>Rom</td>
+                <td>Basel</td>
+                <td>Bella Italia</td>
+                <td>26.12.15</td>
+                <td>29.12.15</td>
+                <td>18</td>
+                <td><button type="button" class="btn btn-default">Bearbeiten</button></td>
             </tr>
             <tr class="danger">
-                <td>Mary</td>
-                <td>Moe</td>
-                <td>mary@example.com</td>
-            </tr>
-            <tr class="info">
-                <td>July</td>
-                <td>Dooley</td>
-                <td>july@example.com</td>
+                <td>0003</td>
+                <td>Basel</td>
+                <td>Zürich</td>
+                <td>Drei König</td>
+                <td>31.12.15</td>
+                <td>1.1.16</td>
+                <td>2</td>
+                <td><button type="button" class="btn btn-default">Bearbeiten</button></td>
             </tr>
         </tbody>
     </table>
