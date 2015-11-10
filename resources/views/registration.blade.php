926602adb1775f79f1e585e4e6d2823880e4dd85
chriskoller08 <chriskoller08@5105edd4-cd02-4104-8b14-f72aa8a08039>
2015-11-10 09:39:02 +0000

    fertigstellung der elemente

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -3,56 +3,118 @@
 <!--content -->
 @section ('content')
 <body>
+    <div class="col-md-12 panel panel-default">
+        <div class="panel panel-default">
+            <div class="panel-heading">Reisedetails</div>
+            <div class="panel-body">Panel Content</div>
+        </div>
+    </div>
     <div class="col-md-4 panel panel-default">
         <div class="well">
             <h2>Personal Data</h2>
             <form role="form">
                 <div class="form-group">
-                    <label for="prename">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    <label for="prename">Vorname</label>
+                    <input type="text" class="form-control" id="prename" placeholder="Vorname">
                 </div>
                 <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Nachname">
                 </div>
-            </form>
+                <div class="form-group">
+                    <label for="adress">Adresse</label>
+                    <input type="text" class="form-control" id="adress" placeholder="Adresse">
+                </div>
+                <div class="form-group">
+                    <label for="PLZ">PLZ</label>
+                    <input type="text" class="form-control" id="PLZ" placeholder="PLZ">
+                </div>
+                <div class="form-group">
+                    <label for="city">Ort</label>
+                    <input type="text" class="form-control" id="city" placeholder="Ort">
+                </div>
+                <div class="form-group">
+                    <label for="tel">Telefonnummer</label>
+                    <input type="text" class="form-control" id="tel" placeholder="Telefonnummer">
+                </div>
+                <div class="form-group">
+                    <label for="email">E-Mail</label>
+                    <input type="text" class="form-control" id="email" placeholder="E-Mail Adresse">
+                </div>
+
 
         </div></div>
     <div class="col-md-4 panel panel-default">
         <div class="well">
-            <h2>Vertical (basic) form</h2>
-            <form role="form">
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+            <h2>Zahlungsart</h2>
+
+            <div class="form-group">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Zahlungsart
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><div class="radio"><label><input type="radio" name="card" value="Mastercard" checked> Mastercard</label></div></li>
+                        <li><div class="radio"><label><input type="radio" name="card" value="Visa" checked> Visa</label></div></li>
+                        <li><div class="radio"><label><input type="radio" name="card" value="PostCard" checked> Post Card</label></div></li>
+                    </ul>
                 </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-                </div>
-                <div class="checkbox">
-                    <label><input type="checkbox"> Remember me</label>
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+            </div>
+            <div class="form-group">
+                <label for="cardnumber">Kartennummer</label>
+                <input type="text" class="form-control" id="cardnumber" placeholder="Kartennummer">
+            </div>
+            <div class="form-group">
+                <label for="securitycode">Security Code</label>
+                <input type="text" class="form-control" id="securitycode" placeholder="Security Code">
+            </div>
+            <div class="form-group">
+                <label for="month">Monat</label>
+                <input type="text" class="form-control" id="month" placeholder="Monat">
+            </div>
+            <div class="form-group">
+                <label for="year">Jahr</label>
+                <input type="text" class="form-control" id="year" placeholder="Jahr">
+            </div>
+            <div class="form-group">
+                <label for="cardName">Name des Karteninhabers</label>
+                <input type="text" class="form-control" id="cardName" placeholder="Vollständiger Name des Karteninhabers">
+            </div>
+            
         </div></div>
-<div class="col-md-4 panel panel-default">
+    <div class="col-md-4 panel panel-default">
         <div class="well">
-            <h2>Vertical (basic) form</h2>
-            <form role="form">
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-                </div>
-                <div class="checkbox">
-                    <label><input type="checkbox"> Remember me</label>
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+            <h2>AGB & Login</h2>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Ich akzeptiere die AGB
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Ich möchte den Newsletter abonnieren
+                </label>
+            </div>
+            <div class="form-group">
+                <label for="nickname">Nickname</label>
+                <input type="text" class="form-control" id="nickname" placeholder="Nickname">
+            </div>
+            <div class="form-group">
+                <label for="password">Passwort</label>
+                <input type="password" class="form-control" id="password" placeholder="Passwort">
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword">Passwort wiederholen</label>
+                <input type="password" class="form-control" id="confirmPassword" placeholder="Passwort">
+            </div>
+
         </div>
     </div>
-    @stop
+    <div class="col-md-4 panel panel-default">
+        <div class="well">
+            <button type="submit" class="btn btn-success">Reise buchen</button>
+            <button type="reset" class="btn btn-danger">Reset</button>
+        </div>
+    </div>
+</form>
+@stop
