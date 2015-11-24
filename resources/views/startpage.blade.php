6234fb96ec50b66e1c0776a7e1d50e63df6522e5
chriskoller08 <chriskoller08@5105edd4-cd02-4104-8b14-f72aa8a08039>
2015-11-24 22:50:48 +0000

    RID geändert

--- resources/views/startpage.blade.php
+++ resources/views/startpage.blade.php
@@ -12,11 +12,11 @@
     </div>
     <div style="display: flex" class="row">
         @include ('templates.content.trip', ['tripName' => 'Rom', 'pics' => ['Rom1', 'Rom2'], 'RID' => '3', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '800$', 'booking' => 'jetzt Name buchen'])
-        @include ('templates.content.trip', ['tripName' => 'Wien', 'pics' => ['Wien1', 'Wien2'], 'RID' => '1', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '500$', 'booking' => 'jetzt Name buchen'])
+        @include ('templates.content.trip', ['tripName' => 'Wien', 'pics' => ['Wien1', 'Wien2'], 'RID' => '4', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '500$', 'booking' => 'jetzt Name buchen'])
     </div>
     <div style="display: flex" class="row">
-        @include ('templates.content.trip', ['tripName' => 'Basel', 'pics' => ['Basel1', 'Basel2'], 'RID' => '2', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '700$', 'booking' => 'jetzt Name buchen'])
-        @include ('templates.content.trip', ['tripName' => 'Rom', 'pics' => ['Rom1', 'Rom2'], 'RID' => '3', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '800$', 'booking' => 'jetzt Name buchen'])
+        @include ('templates.content.trip', ['tripName' => 'Basel', 'pics' => ['Basel1', 'Basel2'], 'RID' => '5', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '700$', 'booking' => 'jetzt Name buchen'])
+        @include ('templates.content.trip', ['tripName' => 'Rom', 'pics' => ['Rom1', 'Rom2'], 'RID' => '6', 'describtion' => 'Text Name', 'data' => 'Daten Name', 'price' => '800$', 'booking' => 'jetzt Name buchen'])
     </div>
 
 </div>
