3bf057d5f5fdbb729948bf9cccbed0b6106316d9
chriskoller08 <chriskoller08@5105edd4-cd02-4104-8b14-f72aa8a08039>
2015-12-08 22:21:17 +0000

    new template for adminoverview

---
+++ resources/views/templates/content/overview.blade.php
@@ -0,0 +1,11 @@
+
+<tr class="info"><!--TODO anpassen an anzahl besucher-->
+    <td>{{$trip->id}}</td>
+    <td>{{$trip->destination}}</td>
+    <td>{{$trip->depart}}</td> <!--TODO, update database with Depart-->
+    <td>Bellvue</td> 
+    <td>{{$trip->startDate}}</td>
+    <td>{{$trip->endDate}}</td>
+    <td>10</td>
+    <td><button type="button" class="btn btn-default">Bearbeiten</button></td>
+</tr>
