71c6f2424c5cc99684e5abee2214579f8fd6a002
lavanchy <lavanchy@5105edd4-cd02-4104-8b14-f72aa8a08039>
2015-12-23 08:42:54 +0000

    müll entsorgt!!!!

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,4 +1,11 @@
 <!--Hier wird die allgemeine View generiert mit Blade. Zunächst wird partials.header geladen, dann der Content der jeweiligen View, dann partials.footer-->
 @include('templates.partials.header')
 @yield('content')
+@if($errors->any())
+<ul class="alert alert-danger">
+    @foreach( $errors->all() as $error)
+    <li> {{error}} </li>
+    @endforeach
+</ul>
+@endIF
 @include('templates.partials.footer')
